85653ae21dd426171801add6be3275facd4e4378
John Moxon <jmoxon@gmail.com>
2020-05-18 11:14:59 +1000

    setting up bulma

---
+++ src/js/contact_form.js
@@ -0,0 +1,38 @@
+/**
+ * contact_form.js
+ * Handles Ajax handling of the simpleform
+ */
+;(function($, window, undefined){
+  $('#simpleform').bind('submit', function(event) {
+    var $this = $(this);
+
+    // Push enquiry information to dataLayer and fire event
+    var enquiryData = {};
+    $.each($this.serializeArray(), function(){
+        enquiryData[this.name] = this.value;
+    });
+
+    dataLayer.push({
+      'event'   : 'enquiry.submit',
+      'enquiry' : enquiryData,
+      'enquiry.email' : enquiryData.email,
+      'enquiry.fullname' : enquiryData.fullname,
+      'enquiry.message' : enquiryData.message
+    });
+
+    $.ajax({
+      dataType: 'jsonp',
+      url: "https://getsimpleform.com/messages/ajax?form_api_token=41dc591ad6a976440d791b04906034c7",
+      data: $this.serialize()
+    }).done(function() {
+      $this.slideUp()
+        .parent()
+        .find('.alert')
+        .removeClass('hide')
+        .hide()
+        .fadeIn(700);
+    });
+
+    event.preventDefault();
+  });
+})(jQuery, window);
---
+++ src/js/main.js
@@ -0,0 +1,87 @@
+/**
+ * main.js
+ * Handles main theme
+ */
+
+;(function($,window){
+
+  var $window = $(window);
+
+  // dataLayer fallback
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push = window.dataLayer.push || function(){ return false; };
+
+  // Register DISQUS comment callback
+  window.disqus_config = function() {
+    this.callbacks.onNewComment = [function(comment) {
+      window.dataLayer.push({
+        'event': 'comment.add',
+        'comment.text': comment.text
+      });
+    }];
+  };
+
+  // Tooltips for share buttons
+  $('[data-toggle="tooltip"]').tooltip();
+
+  //jQuery to collapse the navbar on scroll
+  $window.scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").not('.disable-nav-expanded').addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").not('.disable-nav-expanded').removeClass("top-nav-collapse");
+    }
+
+    // Show scroll to *top* button for posts
+    if ( $('.post-single').length > 0 &&
+      $window.scrollTop() > $('.post-single').offset().top) {
+      if($('.scroll-to-top').length < 1) {
+        $('<span class="page-scroll"><a href="#content" class="scroll-to-top btn btn-default"><i class="fa fa-play-circle fa-rotate-270"></i> top</a></span>')
+          .appendTo('body')
+          .hide()
+          .fadeIn(300);
+      } else {
+        $('.scroll-to-top').css('opacity', 1);
+      }
+    } else {
+      $('.scroll-to-top').css('opacity', 0);
+    }
+  });
+
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+    $('body').delegate('.page-scroll a', 'click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
+
+  // Calculate post reading time
+  $('.post').each(function(){
+    var $this = $(this);
+    $this.find('.eta').text( $this.readingtime( $this.data('words') ) );
+  });
+
+  // fade out movie on scroll
+  var fadeBegin = 10,
+  fadeFinish = 600,
+  coverElement = $('video').first(),
+  fadingElement = $('video').first();
+  $(window).bind('scroll', function(){
+    var offset = $(document).scrollTop(), opacity = 1;
+
+    if( offset <= fadeBegin ){
+      opacity = 1;
+    } else if( offset <= fadeFinish ){
+      opacity = 1 - offset / fadeFinish;
+    } else {
+      opacity = 0;
+    }
+    fadingElement.css('opacity',opacity);
+  });
+
+})(jQuery, window, undefined);
+/** End JM script **/
