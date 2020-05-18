9c6d179ee9c208a6f5f848b8ee2007a47a5471f0
John Moxon <jmoxon@gmail.com>
2020-05-18 11:14:59 +1000

    setting up bulma

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,49 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
+    app: './src/index/js/index.js',
+    vendor: './src/index/js/vendor.js'
   },
-  plugins:[
+  devtool: false,
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map',
+      exclude: ['vendor.bundle.js']
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
+      hash: true,
+      template: './src/index/index.html',
+      filename: 'index.html'
     }),
   ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[sc]ss$/i,
+        use: [
+          'style-loader', // create `style` nodes from JS strings
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ]
+      },
+    ],
+  },
+  // plugins:[
+  // ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 };
+//
