01ad2484ffe29ad0a8dbf654199b9ad21e18af25
John Moxon <jmoxon@gmail.com>
2020-05-19 16:53:21 +1000

    base theme

--- src/index/js/index.js
+++ src/index/js/index.js
@@ -1,2 +1,22 @@
 //  Import custom styles
 import '../sass/base.scss';
+
+
+// Enable mobile nav banner
+document.addEventListener('DOMContentLoaded', () => {
+
+  // If there are any navbar burgers add a click event to each to toggle classes
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+        const $target = document.getElementById(el.dataset.target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      })
+    });
+  }
+
+
+
+});
