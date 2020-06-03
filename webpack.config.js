822220e9b1222e6da59b4fab659d8aeb2a10a647
John Moxon <jmoxon@gmail.com>
2020-06-04 09:03:51 +1000

    starting template separation

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,7 @@ module.exports = {
   // ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/js/'
   },
 };
